365970bbcb6bda24dbc263e71c18101d16ed258c
qkrtkdgus010208 <psh010208@naver.com>
2025-07-19 00:58:17 +0900

    SPUM 연습
    1. 캐릭터 컨트롤 스크립트를 만듦
    2. 공격 애니메이션이 기본이 아닌 스킬로 나가는 오류 수정

---
+++ Assets/Scripts/move_test.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class move_test : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Vector2 moveInput = new Vector2(moveX, moveY).normalized;
+
+        rb.linearVelocity = moveInput * 5f;
+
+        animator.SetBool("1_Move", moveInput != Vector2.zero);
+
+        // 좌우 반전
+        if (moveX > 0) transform.localScale = new Vector3(-1, 1, 1);
+        else if (moveX < 0) transform.localScale = new Vector3(1, 1, 1);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("2_Attack");
+        }
+    }
+}
